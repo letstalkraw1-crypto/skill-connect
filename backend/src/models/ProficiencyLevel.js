3c17da2fda646e5d7f67bbfa6b9f63431c5d59f3
Coderdubey <dubey@example.com>
2026-04-15 18:56:30 +0530

    chore: remove unused files - test scripts, old models, documentation clutter

--- backend/src/models/ProficiencyLevel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const proficiencyLevelSchema = new mongoose.Schema({
-  _id: { type: mongoose.Schema.Types.ObjectId, default: () => new mongoose.Types.ObjectId() },
-  name: { type: String, unique: true, required: true }
-});
-
-module.exports = mongoose.model('ProficiencyLevel', proficiencyLevelSchema);
